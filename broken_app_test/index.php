c07a7bc83f610245cc917226a9789ee805284403
Giovanni Colasante <giovanni.colasante@gmail.com>
2022-12-06 16:54:41 +0100

    Organize test page

--- broken_app_test/index.php
+++ broken_app_test/index.php
@@ -4,15 +4,20 @@ use Symfony\Component\HttpFoundation\Response;
 
 require __DIR__.'/../bootstrap.php';
 
+$messages = [
+    "Dati generati con : https://www.fakedata-generator.com",
+];
+
 $users = $db->fetchAllAssociative('SELECT * FROM user');
 
 $params = [
     'page_title' => 'Controllo funzionamento',
     'users'      => $users,
+    'messages'   => $messages,
 ];
 
 $response = new Response();
-$response->setContent($twig->render('test_working-index.twig', $params));
+$response->setContent($twig->render('broken_app_test-index.twig', $params));
 $response->setStatusCode(Response::HTTP_OK);
 $response->headers->set('Content-Type', 'text/html');
 $response->send();
